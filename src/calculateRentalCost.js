4f8481bce954b1f3321e3978cfe36b8985b886b2
Vladyslav Vyshnivskyi <vladyslav.vyshnivskyi@avanquest.com>
2026-06-30 11:46:56 +0300

    Add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const costPerDay = 40;
+  let totalCost = costPerDay * days;
+
+  if (days >= 3) {
+    totalCost -= 20;
+  }
+
+  if (days >= 7) {
+    totalCost -= 30;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
