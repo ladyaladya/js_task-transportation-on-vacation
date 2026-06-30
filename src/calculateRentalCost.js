35d9a8fb7a52b3e6e1dcb3d2f219310157bf8cdc
Vladyslav Vyshnivskyi <vladyslav.vyshnivskyi@avanquest.com>
2026-06-30 12:01:21 +0300

    Update solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -8,15 +8,15 @@ function calculateRentalCost(days) {
   const SHORT_TERM_DAYS = 3;
   const SHORT_TERM_DISCOUNT = 20;
   const LONG_TERM_DAYS = 7;
-  const LONG_TERM_DISCOUNT = 30;
-  let totalCost = COST_PER_DAY * days;
+  const LONG_TERM_DISCOUNT = 50;
+  const totalCost = COST_PER_DAY * days;
 
-  if (days >= SHORT_TERM_DAYS) {
-    totalCost -= SHORT_TERM_DISCOUNT;
+  if (days >= SHORT_TERM_DAYS && days < LONG_TERM_DAYS) {
+    return totalCost - SHORT_TERM_DISCOUNT;
   }
 
   if (days >= LONG_TERM_DAYS) {
-    totalCost -= LONG_TERM_DISCOUNT;
+    return totalCost - LONG_TERM_DISCOUNT;
   }
 
   return totalCost;
